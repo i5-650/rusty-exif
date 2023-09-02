c8737029c2a4e7d09d9e8d7b4037e57865aa8a5f
loic-prn <l.perrin@proton.me>
2023-09-02 19:59:10 +0200

    Optimisation 🚀 + Cleaning 🧹

--- src/exif_mapper/mod.rs
+++ src/exif_mapper/mod.rs
@@ -4,71 +4,78 @@ use sscanf::scanf;
 
 use self::data_structures::Image;
 
-mod data_structures;
+pub mod data_structures;
+
+const GOOGLE_MAP: &str = "googleMap";
 
 pub fn map_exif_from_file(_path_to_image: PathBuf) -> HashMap<String, String> {
 	let mut map_data = HashMap::new();
 
-	if let Ok(file) = std::fs::File::open(_path_to_image.as_path()) {
-		let mut bufreader = std::io::BufReader::new(&file);
-		let exifreader = exif::Reader::new();
+	let file = std::fs::File::open(_path_to_image.as_path()).expect("Couldn't open the image");
+	let mut bufreader = std::io::BufReader::new(&file);
+	let exifreader = exif::Reader::new();
 
-		if let Ok(exif) = exifreader.read_from_container(&mut bufreader) {
+	if let Ok(exif) = exifreader.read_from_container(&mut bufreader){
 
-			exif.fields()
-				.map(|f| (f.display_value().to_string(), f.tag.to_string()))
-				.for_each(|f| {
-					let mut value = f.0;
-					if value.starts_with("\"") && value.ends_with("\"") {
-						value = value.strip_prefix("\"").unwrap().strip_suffix("\"").unwrap().to_string();
-					}
-					map_data.insert(f.1, value);
-				});
+		exif.fields()
+		.map(|f| (f.tag.to_string(), f.display_value().to_string()))
+		.for_each(|f| {
+			let (key, value) = f;
+			map_data.insert(key, format_value(value));
 
-			if map_data.contains_key("GPSLatitude") && map_data.contains_key("GPSLongitude") {
-				map_data.insert(
-					"googleMap".to_string(), 
-					format!("https://www.google.com/maps/search/?api=1&query={},{}",
-						 convert_dms_to_decimal(map_data.get("GPSLatitude").unwrap()), 
-						 convert_dms_to_decimal(map_data.get("GPSLongitude").unwrap())
-						)
-				);
-			}
-		}
+		});
+	}
+	return add_google_map(map_data);
+}
+
+fn add_google_map(mut map_data :HashMap<String, String>) -> HashMap<String, String> {
+	if map_data.contains_key("GPSLatitude") && map_data.contains_key("GPSLongitude") {
+		map_data.insert(
+			GOOGLE_MAP.to_string(), 
+			format!("https://www.google.com/maps/search/?api=1&query={},{}",
+					convert_dms_to_decimal(map_data.get("GPSLatitude").unwrap()), 
+					convert_dms_to_decimal(map_data.get("GPSLongitude").unwrap())
+				));
 	}
 	return map_data;
 }
 
-pub fn convert_dms_to_decimal(dms: &String) -> f64 {
+fn format_value(input: String) -> String {
+	return input.replace("\"", "").to_string();
+}
+
+fn convert_dms_to_decimal(dms: &String) -> f64 {
 	let parsed = scanf!(dms, "{} deg {} min {} sec", f64, f64, f64).unwrap();
-	let degrees = parsed.0;
-	let minutes = parsed.1;
-	let seconds = parsed.2;
+	let (degrees, minutes, seconds) = parsed;
 	return degrees + minutes / 60.0 + seconds / 3600.0;
 }
 
 pub fn map_exif_from_folder(_path: PathBuf) -> Vec<Image> {
-	let files_list = get_file_list(_path);
+	let files_list = list_files_in_dir(_path);
 	let mut data_out = vec![];
 
-	for pathbuf_file in files_list {
-		data_out.push(data_structures::Image { 
-			name: pathbuf_file.as_path().display().to_string(), 
-			exifs: map_exif_from_file(pathbuf_file) }
-		);
-	}
+	files_list.iter().for_each(|file|{
+		data_out.push(data_structures::Image {
+			name: file.as_path().display().to_string(),
+			exifs: map_exif_from_file(file.to_owned())
+		});
+	});
 	return data_out;
 }
 
 
-fn get_file_list(_path: PathBuf) ->  Vec<PathBuf>{
+fn list_files_in_dir(path: PathBuf) ->  Vec<PathBuf>{
 	let mut file_list = vec![];
 
-	for file in fs::read_dir(_path).unwrap() {
-		if !file.as_ref().unwrap().path().ends_with(".DS_Store") && !file.as_ref().unwrap().path().ends_with("/") {
-			file_list.push(
-				file.unwrap().path()
-			);
+	let files = fs::read_dir(path).expect("Couldn't read the directory given");
+
+	for file in files {
+		let path_file = file.as_ref()
+			.expect("Couldn't get the ref on a file in the directory")
+			.path();
+
+		if !path_file.ends_with(".DS_Store") && !path_file.ends_with("/") {
+			file_list.push(path_file);
 		}
 	}
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use clap::{ArgGroup, Parser};
-use std::fs::File;
-use rayon::prelude::{ParallelSliceMut, IntoParallelRefMutIterator, ParallelIterator};
 /**
  * Licence: MIT
  * Author: loic-prn
  * Date: 2022-11-19
  *
  */
+
+use std::any::Any;
+use std::collections::HashMap;
+use clap::{ArgGroup, Parser};
+use std::fs::File;
+use std::println;
+use rayon::prelude::{ParallelSliceMut, IntoParallelRefMutIterator, ParallelIterator};
 use std::io::Write;
 use std::path::PathBuf;
+use exif_mapper::data_structures::Image;
 
 pub mod exif_mapper;
 
@@ -20,7 +25,7 @@ pub mod exif_mapper;
 		.args(["file", "folder"])
 ))]
 struct Args {
-	#[arg(short, long)]
+	#[arg(short = 'e', long = "export")]
 	export: Option<String>,
 	file: Option<PathBuf>,
 	#[arg(short = 'f', long = "folder")]
@@ -29,53 +34,86 @@ struct Args {
 	separate: bool,
 }
 
-fn main() {
-	let args = Args::parse();
+impl Args {
+	pub fn export_and_file(&self) -> bool{
+		return self.export.is_some() && self.file.is_some();
+	}
 
-	if args.file.is_some() && args.export.is_some() {
-		let predic = check_extension(&mut args.export.unwrap());
-		let mut output_file = predic.unwrap();
-		let data_to_jsonify = exif_mapper::map_exif_from_file(PathBuf::from(args.file.unwrap()));
-		let output = serde_json::to_string_pretty(&data_to_jsonify);
-		if !output_file.write(output.unwrap().as_bytes()).is_ok() {
-			println!("Couln't save file...");
-		}
-	} else if args.file.is_some() {
-		for (key, value) in exif_mapper::map_exif_from_file(PathBuf::from(args.file.unwrap())) {
-			println!("{}: {}", key, value);
-		}
-	} else if args.export.is_some() && args.folder.is_some() {
-		let predic = check_extension(&mut args.export.unwrap());
-		let mut output_file = predic.unwrap();
-		let data_to_jsonify =
-			exif_mapper::map_exif_from_folder(PathBuf::from(args.folder.unwrap()));
-		let output = serde_json::to_string_pretty(&data_to_jsonify);
-		if !output_file.write(output.unwrap().as_bytes()).is_ok() {
-			println!("Couln't save file...");
-		}
-	} else if args.separate && args.folder.is_some() {
-		let mut data_to_jsonify =
-			exif_mapper::map_exif_from_folder(PathBuf::from(args.folder.unwrap()));
+	pub fn file_no_export(&self) -> bool {
+		return self.file.is_some() && self.export.is_none();
+	}
 
+	pub fn folder_no_split(&self) -> bool {
+		return self.folder.is_some() && !self.separate;
+	}
 
-		data_to_jsonify.as_parallel_slice_mut().par_iter_mut().for_each(|img| {
-			let output = serde_json::to_string_pretty(&img);
-			img.name.push_str(".json");
-			let file = File::create(&img.name);
-
-			if !file.unwrap().write(output.unwrap().as_bytes()).is_ok() {
-				println!("Couldn't write file");
-			}
-		});
-
-	} else {
-		println!("No file or folder specified, use -h for help");
+	pub fn folder_split(&self) -> bool {
+		return self.folder.is_some() && self.separate;
 	}
 }
 
-fn check_extension(filename: &mut String) -> Result<File, std::io::Error> {
+fn main() {
+	let args = &Args::parse();
+
+	if args.export.is_some() || args.folder_no_split() {
+		let filename = args.export.as_deref().expect("Couldn't get the export filename");
+		let mut output_file = check_extension(&mut filename.to_string());
+		
+		let to_write :Box<dyn Any>;
+
+		if args.export_and_file() {
+			let file_par = args.file.as_deref().expect("Invalid file given as parameter");
+			let image_path = PathBuf::from(file_par);
+			to_write = Box::new(exif_mapper::map_exif_from_file(image_path));
+
+		} else {
+			let folder_par = args.folder.as_deref().expect("Couldn't get the input folder");
+			let folder_path = PathBuf::from(folder_par);
+			to_write = Box::new(exif_mapper::map_exif_from_folder(folder_path));
+		};
+
+		if let Some(map) = to_write.downcast_ref::<HashMap<String, String>>() {
+			let serialized = serde_json::to_string_pretty(map).expect("Couldn't serialize data");
+			output_file.write(serialized.as_bytes()).expect("Couldn't write data in the file.");
+		} else if let Some(list_img) = to_write.downcast_ref::<Vec<Image>>() {
+			let serialized = serde_json::to_string_pretty(list_img).expect("Couldn't serialize data");
+			output_file.write(serialized.as_bytes()).expect("Couldn't write data in the file.");
+		}
+
+		return;
+	}
+
+	if args.file_no_export() {
+		let file_par = args.file.as_deref().expect("Invalid file given as parameter");
+		let image_path = PathBuf::from(file_par);
+		let to_print= exif_mapper::map_exif_from_file(image_path);
+		for (key, value) in to_print {
+			println!("{}: {}", key, value);
+		}
+		return;
+	}
+
+	if args.folder_split() {
+		let folder_par = args.folder.as_deref().expect("Couldn't get the input folder");
+		let folder_path = PathBuf::from(folder_par);
+		let mut to_write = exif_mapper::map_exif_from_folder(folder_path);
+		to_write.as_parallel_slice_mut()
+			.par_iter_mut()
+			.for_each(|img| {
+				let serialized = serde_json::to_string_pretty(&img).expect("Couldn't serialize data");
+				img.name.push_str(".json");
+				let mut output_file = File::create(&img.name).expect("Coulnd't create an output file named as one of the input files");
+				output_file.write(serialized.as_bytes()).expect("Couldn't write data in the file.");
+			});
+		return;
+	}
+
+	println!("Something went wrong with parameters");
+}
+
+fn check_extension(filename: &mut String) -> File {
 	if !filename.ends_with(".json") {
 		filename.push_str(".json");
 	}
-	return File::create(filename);
+	return File::create(filename).expect("Couldn't create the ouput file.");
 }
